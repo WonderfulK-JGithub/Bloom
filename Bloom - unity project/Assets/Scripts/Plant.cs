d6e596aec78ae58f09a7d65428b803b8724d9a0c
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2023-01-12 16:18:58 +0100

    mr bruh

---
+++ Bloom - unity project/Assets/Scripts/Plant.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour,IWaterable
+{
+    [SerializeField] float meshHeight;
+    [SerializeField] Material mat;
+    [SerializeField] float fillPerShoot;
+    [SerializeField] float fillSpeed;
+
+    float waterValue;
+    public float WaterValue
+    {
+        get
+        {
+            return waterValue;
+        }
+        set
+        {
+            waterValue = Mathf.Clamp(value, 0f, 1f);
+        }
+    }
+
+    float suspect = 0.7f;
+    float shaderValue;
+
+    private void Awake()
+    {
+        mat.SetFloat("_Height", meshHeight);
+        shaderValue = -suspect;
+    }
+
+    private void Update()
+    {
+        //shaderValue = Mathf.MoveTowards(shaderValue, WaterValue * suspect * 2f - suspect,fillSpeed * Time.deltaTime);
+        shaderValue = Mathf.Lerp(shaderValue, WaterValue * suspect * 2f - suspect,  fillSpeed * Time.deltaTime * 60f);
+        mat.SetFloat("_Value", shaderValue);
+    }
+
+    public void Water()
+    {
+        WaterValue += fillPerShoot;
+    }
+}
