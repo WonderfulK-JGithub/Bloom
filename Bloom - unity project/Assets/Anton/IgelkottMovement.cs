203c4242f615cc0f61d664a28017b74b54e2a87e
angulan <79506187+angulan@users.noreply.github.com>
2023-01-17 09:32:48 +0100

    det jag gjorde igår

--- Bloom - unity project/Assets/Anton/IgelkottMovement.cs
+++ Bloom - unity project/Assets/Anton/IgelkottMovement.cs
@@ -7,9 +7,11 @@ public class IgelkottMovement : enemymovement
     [Header("Hedgehog specific ---------")]
     public float attackRange = 3;
     bool atPlayer;
+    bool lastatPlayer;
 
     protected override void Movement()
     {
+        lastatPlayer = atPlayer;
         atPlayer = distanceToPlayer < attackRange;
 
         if (chase)
@@ -18,8 +20,10 @@ public class IgelkottMovement : enemymovement
             {
                 StopCoroutine(wander);
             }
-
-            rb.velocity = (transform.forward * moveSpeed) + Physics.gravity * System.Convert.ToInt32(!onGround) * System.Convert.ToInt32(!atPlayer);
+            if (!atPlayer)
+            {
+                rb.velocity = (transform.forward * moveSpeed) + Physics.gravity * System.Convert.ToInt32(!onGround);
+            }
         }
         else
         {
@@ -27,6 +31,25 @@ public class IgelkottMovement : enemymovement
             {
                 wander = StartCoroutine(Wander());
             }
+        }
+    }
+    protected override void Rotation()
+    {
+        if (chase)
+        {
+            if (atPlayer)
+            {
+                rb.angularVelocity = Vector3.zero;
+                Quaternion fullRot = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((target.position - transform.position).normalized), Time.deltaTime * rotationSpeed);
+                rb.rotation = Quaternion.Euler(new Vector3(rb.rotation.x, fullRot.eulerAngles.y, rb.rotation.z));
+            }
+            else
+            {
+                rb.angularVelocity = Vector3.zero;
+                Quaternion fullRot = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((target.position - transform.position).normalized), Time.deltaTime * rotationSpeed);
+                rb.rotation = Quaternion.Euler(new Vector3(rb.rotation.x, fullRot.eulerAngles.y, rb.rotation.z));
+            }
+            
         }
     }
 }
