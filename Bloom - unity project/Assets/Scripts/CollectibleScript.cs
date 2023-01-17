13897199f6175b629bd7a0d49d600c0be9d21925
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2023-01-17 11:53:37 +0100

    trash 2

--- Bloom - unity project/Assets/Scripts/CollectibleScript.cs
+++ Bloom - unity project/Assets/Scripts/CollectibleScript.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class CollectibleScript : MonoBehaviour
 {
     [SerializeField] List<GameObject> trash = new List<GameObject>();
+    [SerializeField] float rotationSpeed;
+
+    float susDomesticus;
 
     private void Start()
     {
         GameObject go = Instantiate(trash[Random.Range(0, trash.Count)], transform);
         go.transform.localScale = Vector3.one * 0.3f;
+    }
+
+    private void Update()
+    {
+        susDomesticus += Time.deltaTime * rotationSpeed;
+        transform.rotation = Quaternion.Euler( new Vector3(0f, susDomesticus, -45f));
     }
 
     public void Collect()
