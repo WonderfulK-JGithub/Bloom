3ec9b6e438fd284a5ffcaa0c7d7a21703611ae5b
Max <79506271+max-aug@users.noreply.github.com>
2023-02-01 10:37:28 +0100

    alla changes

--- Bloom - unity project/Assets/Scripts/Player/CollectibleCollectorScript.cs
+++ Bloom - unity project/Assets/Scripts/Player/CollectibleCollectorScript.cs
@@ -12,20 +12,43 @@ public class CollectibleCollectorScript : PlayerBaseScript
     [SerializeField] GameObject recycleText;
     [SerializeField] TextMeshProUGUI xOfxCansText;
     [SerializeField] GameObject upgradeText;
-    [SerializeField] int neededForUpgrade;
+    int onLevel;
+    [SerializeField] int neededForUpgrades;
     [SerializeField] GameObject biggestBird;
+    [SerializeField] Slider upgradeSlider;
+
+    [Header("Lathet")]
+    [SerializeField] GameObject[] checkmarks;
 
 
     int collectibles = 0;
-    int recycled;
+    int recycled = 0;
+    int totalRecycled = 0;
 
     TextMeshProUGUI collectibleText;
 
     public override void Awake()
     {
+        base.Awake();
+
         collectibleText = GameObject.Find("CollectibleText").GetComponent<TextMeshProUGUI>();
 
-        neededForUpgrade = FindObjectsOfType<CollectibleScript>().Length;
+        onLevel = FindObjectsOfType<CollectibleScript>().Length;
+    }
+
+    private void Start()
+    {
+        totalRecycled = PlayerPrefs.GetInt("TotalRecycled");
+        CheckUpgrades();
+        h.Damage(-100);
+        shooting.FillAmmo();
+
+        Debug.Log(totalRecycled);
+
+        if (totalRecycled == 0)
+            upgradeSlider.transform.parent.gameObject.SetActive(false);
+        else
+            Invoke(nameof(TurnOffSlider), 3);
     }
 
     private void Update()
@@ -39,6 +62,9 @@ public class CollectibleCollectorScript : PlayerBaseScript
                 _recycle.Recycle();
 
                 recycled += collectibles;
+                totalRecycled += collectibles;
+                PlayerPrefs.SetInt("TotalRecycled", totalRecycled);
+                upgradeSlider.transform.parent.gameObject.SetActive(true);
 
                 collectibles = 0;
                 collectibleText.text = collectibles.ToString();
@@ -64,9 +90,11 @@ public class CollectibleCollectorScript : PlayerBaseScript
 
     public void RecycleComplete()
     {
-        if(recycled < neededForUpgrade)
+        CheckUpgrades();
+
+        if (recycled < onLevel)
         {
-            xOfxCansText.text = recycled.ToString() + "/" + neededForUpgrade.ToString();
+            xOfxCansText.text = recycled.ToString() + "/" + onLevel.ToString();
             xOfxCansText.gameObject.SetActive(true);
             Invoke(nameof(Mogus), 2f);
         }
@@ -82,5 +110,23 @@ public class CollectibleCollectorScript : PlayerBaseScript
     {
         xOfxCansText.gameObject.SetActive(false);
         upgradeText.SetActive(false);
+    }
+
+    void CheckUpgrades()
+    {
+        float percent = (float)totalRecycled / (float)neededForUpgrades;
+        Debug.Log(percent);
+        upgradeSlider.value = percent;
+        if (percent >= 0.25f) { h.SetMaxHealth(150); checkmarks[0].SetActive(true); }
+        if (percent >= 0.5f) { shooting.SetMaxAmmo(150); checkmarks[1].SetActive(true); }
+        if (percent >= 0.75f) { h.SetMaxHealth(200); checkmarks[2].SetActive(true); }
+        if (percent >= 1f) { shooting.SetMaxAmmo(200); checkmarks[3].SetActive(true); }
+
+        Invoke(nameof(TurnOffSlider), 5);
+    }
+
+    void TurnOffSlider()
+    {
+        upgradeSlider.transform.parent.gameObject.SetActive(false);
     }
 }
